ee8ae20627cc78ad96f95ce0192ff53c1b10f162
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-11 13:10:32 +0300

    Add files via upload

---
+++ seventhlesson.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char myNumber=99;
+    printf("Karakterimizin sayısal degeri:%d\n",myNumber);
+    printf("Karekterimizin ASCII degeri:%c\n",myNumber);
+    return 0;
+
+
+
+
+
+
+}
