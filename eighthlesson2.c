1c28354bac8b07c423c3c095cabd94875b70c222
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-11 13:15:39 +0300

    Add files via upload

---
+++ eighthlesson2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int number1,number2,number3;
+    number1=121-12;
+    number2=number1+12;
+    number3=number2*number1;
+    printf("Number 1 is= %d\n",number1);
+    printf("Number 2 is= %d\n",number2);
+    printf("Number 3 is= %d\n",number3);
+
+    return 0;
+}
