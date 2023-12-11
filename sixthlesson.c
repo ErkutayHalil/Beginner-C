f7abbc779e1e795168ddc458eab2ceba4d9b5484
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-11 13:06:30 +0300

    Add files via upload
    How to use 'scanf'

---
+++ sixthlesson.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int myNumber,myNumber2;
+    printf("Enter two numbers:");
+    scanf("%d %d",&myNumber,&myNumber2);
+    printf("The number you entered is %d - %d",myNumber,myNumber2);
+    return 0;
+
+
+}
