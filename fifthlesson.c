9a732a2aad6ac64f2873e5d2ac019fd91c3b5009
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-11 13:04:08 +0300

    Add files via upload

---
+++ fifthlesson.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char myLetter = 'A';
+    int myNumber = 47;
+    float myNumber2 = 5.32;
+    double myNumber3 = 321.943;
+
+    printf("my favorite letter is %c\n", myLetter);
+    printf("my lucky number is %d\n", myNumber);
+    printf("%f\n", myNumber2);
+    printf("%lf\n", myNumber3);
+    printf("%s\n","where have you been?");
+
+    return 0;
+
+
+
+
+
+
+}
