41ee8c0e13cb5ffdf33ecd17660769979b2991ea
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-11 13:00:51 +0300

    Add files via upload

---
+++ forthlesson.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char myLetter = 'A';
+    int myNumber = 47;
+    float myNumber2 = 5.32;
+    double myNumber3 = 321.943;
+
+    printf("%c\n", myLetter);
+    printf("%d\n", myNumber);
+    printf("%f\n", myNumber2);
+    printf("%lf\n", myNumber3);
+    
+
+    return 0;
+}
