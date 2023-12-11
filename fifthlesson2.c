1058205124cb7a44feb303dd6be6a8b28ebe64e0
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-11 13:04:42 +0300

    Add files via upload

---
+++ fifthlesson2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int myNumber,myNumber2,myNumber3,sum;
+    myNumber = 7;
+    myNumber2 = 19;
+    myNumber3 = 134;
+    sum = myNumber + myNumber2 + myNumber3;
+    printf("sum: %d\n", sum);
+    
+    return 0;
+}
