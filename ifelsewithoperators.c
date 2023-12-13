e46ac98a73810fb469919bb3114188890c4389e9
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-13 18:00:03 +0300

    Add files via upload
    How to use if else statements with logical operators?

---
+++ ifelsewithoperators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    number = 7;
+
+    if(number % 2 == 0)
+    {
+        printf("The number is even.\n");
+    }
+    else
+    {
+        printf("The number is odd.\n");
+    }
+
+    return 0;
+
+}
