e6ea15dc54e076a5306b8a68693bd444c005fd98
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-14 16:06:35 +0300

    Add files via upload
    How to write fibonacci series with for loop?

---
+++ fibonacciseries.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,i;
+    
+    a=1;
+    b=1;
+
+    printf("%d\n",a);
+    printf("%d\n",b);
+
+    for(i=1;i<=8;i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+
+        printf("%d\n",c);
+    }
+
+    return 0;
+
+}
