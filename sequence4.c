f17f14f4a1228b3ed047c57dfb5ba6f821d37429
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-16 21:34:10 +0300

    Add files via upload
    How to write the numbers by using sequence statement?

---
+++ sequence4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    int numbers[4];
+    numbers[0]=45;
+    numbers[1]=890;
+    numbers[2]=61;
+    numbers[3]=2341;
+    printf("%d",numbers[2]);
+
+    return 0;
+}
