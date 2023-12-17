b49b75f1596dd4c01bd1d5bf4b2fd3ff920e344b
ErkutayHalil <153088605+ErkutayHalil@users.noreply.github.com>
2023-12-17 11:10:16 +0300

    Add files via upload
    How to use sequences with for loop?

---
+++ sequence5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int numbers[]={10,20,30,40,50,60,70,80,90,100};
+
+    int i;
+
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n",numbers[i]);
+    }
+
+    return 0;
+
+}
