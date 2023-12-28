1ad1ab442211fbfc41e677233096e8304f8c1aff
ErkutayHalil <erkomut1@gmail.com>
2023-12-28 22:41:42 +0300

    Add files via upload
    How to use puts() and gets() functions?

---
+++ putsgets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char info[40];
+    printf("Enter the info: ");
+    gets(info);
+    printf("\n\n");
+    puts(info);
+
+    return 0;
+}
