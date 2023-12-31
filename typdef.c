655e7487bfce3f63d730012c542456d851a323a6
ErkutayHalil <erkomut1@gmail.com>
2023-12-31 11:58:02 +0300

    Add files via upload
    How to use typedef?

---
+++ typdef.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    typedef int evennumber;
+
+    int main()
+    {
+        evennumber en;
+        en = 24;
+        printf("%d",en);
+
+        return 0;
+    }
