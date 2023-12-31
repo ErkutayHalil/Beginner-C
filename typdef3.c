21009a56afca76f8d8b1297c0feb8a7a5ad8affa
ErkutayHalil <erkomut1@gmail.com>
2023-12-31 12:08:06 +0300

    Add files via upload
    How to find sum of the numbers by using typedef?

---
+++ typdef3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    typedef int value1;
+
+    int main()
+    {
+        int sum;
+        value1 value2;
+        value1 value3;
+        value2 = 25;
+        value3 = 32;
+        sum = value2 + value3;
+
+        printf("Sum of the numbers: %d",sum);
+
+        return 0;
+    }
