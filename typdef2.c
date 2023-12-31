74f1ae9859e3259bf6fe3cd4ae1a6a1c5e0c47d3
ErkutayHalil <erkomut1@gmail.com>
2023-12-31 12:01:54 +0300

    Add files via upload
    How to print multiple numbers by using typedef?

---
+++ typdef2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    typedef int evennumber;
+
+    int main()
+    {
+        evennumber en;
+        en = 24;
+        evennumber en2,en3;
+        en2 = 26;
+        en3 = 28;
+        printf("%d %d %d",en,en2,en3);
+
+        return 0;
+    }
