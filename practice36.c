43e42efc815975c1765da1abb32208866079d437
ErkutayHalil <erkomut1@gmail.com>
2024-01-25 13:10:06 +0300

    Add files via upload
    This code prints letters from the English alphabet.(switch case)

---
+++ practice36.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+int main()
+{
+    char letter;
+    printf("Enter a letter: ");
+    scanf("%c",&letter);
+    switch(letter)
+    {
+        case 'a':
+        printf("You pushed the a button.");
+        break;
+        case 'b':
+        printf("You pushed the b button.");
+        break;
+        case 'c':
+        printf("You pushed the c button.");
+        break;
+        case 'd':
+        printf("You pushed the d button.");
+        break;
+        case 'e':
+        printf("You pushed the e button.");
+        break;
+        case 'f':
+        printf("You pushed the f button.");
+        break;
+        case 'g':
+        printf("You pushed the g button.");
+        break;
+        case 'h':
+        printf("You pushed the h button.");
+        break;
+        case 'i':
+        printf("You pushed the i button.");
+        break;
+        case 'j':
+        printf("You pushed the j button.");
+        break;
+        case 'k':
+        printf("You pushed the k button.");
+        break;
+        case 'l':
+        printf("You pushed the l button.");
+        break;
+        case 'm':
+        printf("You pushed the m button.");
+        break;
+        case 'n':
+        printf("You pushed the n button.");
+        break;
+        case 'o':
+        printf("You pushed the o button.");
+        break;
+        case 'p':
+        printf("You pushed the p button.");
+        break;
+        case 'q':
+        printf("You pushed the q button.");
+        break;
+        case 'r':
+        printf("You pushed the r button.");
+        break;
+        case 's':
+        printf("You pushed the s button.");
+        break;
+        case 't':
+        printf("You pushed the t button.");
+        break;
+        case 'u':
+        printf("You pushed the u button.");
+        break;
+        case 'v':
+        printf("You pushed the v button.");
+        break;
+        case 'w':
+        printf("You pushed the w button.");
+        break;
+        case 'x':
+        printf("You pushed the x button.");
+        break;
+        case 'y':
+        printf("You pushed the y button.");
+        break;
+        case 'z':
+        printf("You pushed the z button.");
+        break;
+        default:
+        printf("Please enter a letter from english alphabet.");
+
+    }
+}
